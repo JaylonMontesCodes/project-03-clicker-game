b01a7d5361e5d17356aa909239d49a026e75f3f6
JaylonMontesCodes <jaylonmontes1213@gmail.com>
2026-01-08 20:39:21 -0500

    Add countdown and start game functionality

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
 let score = 0;
 let timeLeft = 10;
 let gameActive = true;
+let countdown = 3;
+
 
 // ELEMENTS
 const scoreDisplay = document.getElementById("score");
@@ -13,22 +15,48 @@ const button = document.getElementById("clickBtn");
 const message = document.getElementById("message");
 
 // CLICK BUTTON LOGIC
-button.addEventListener("click", function () {
-  if (gameActive) {
-    score++;
-    scoreDisplay.textContent = "Score: " + score;
+startBtn.addEventListener("click", function () {
+  if (nameInput.value.trim() === "") {
+    alert("Please enter your name first 🙂");
+    return;
   }
+
+  playerName = nameInput.value;
+  gameStarted = true;
+
+  startBtn.disabled = true;
+  nameInput.disabled = true;
+
+  message.textContent = "Get ready... ⏱️";
+
+  countdown = 3;
+  message.textContent = countdown;
+
+  const countdownTimer = setInterval(function () {
+    countdown--;
+
+    if (countdown > 0) {
+      message.textContent = countdown;
+    } else {
+      clearInterval(countdownTimer);
+      message.textContent = "GO! 🚀";
+      startGame();
+    }
+  }, 1000);
 });
 
 // TIMER LOGIC
-const timer = setInterval(function () {
-  if (timeLeft > 0) {
-    timeLeft--;
-    timeDisplay.textContent = "Time Left: " + timeLeft;
-  } else {
-    endGame();
-  }
-}, 1000);
+function startTimer() {
+  const timer = setInterval(function () {
+    if (timeLeft > 0) {
+      timeLeft--;
+      timeDisplay.textContent = "Time Left: " + timeLeft;
+    } else {
+      clearInterval(timer);
+      endGame();
+    }
+  }, 1000);
+}
 
 // END GAME FUNCTION
 function endGame() {
@@ -36,5 +64,17 @@ function endGame() {
   button.disabled = true;
   message.textContent = "Game Over! Final Score: " + score;
   clearInterval(timer);
+}
+function startGame() {
+  gameActive = true;
+  button.disabled = false;
+
+  score = 0;
+  timeLeft = 10;
+
+  scoreDisplay.textContent = "Score: 0";
+  timeDisplay.textContent = "Time Left: 10";
+
+  startTimer();
 }
 
